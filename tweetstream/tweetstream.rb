99e82bc7ce9c0e32cc9abb742721aff1f2272b42
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-09-15 11:39:09 +0100

    second commit

--- tweetstream/tweetstream.rb
+++ tweetstream/tweetstream.rb
@@ -1,48 +1,54 @@
 ##Streaming
-# # require 't'
 require 'tweetstream'
+require 'json'
+require 'highline/import'
+require 'rubygems'
 
- TweetStream.configure do |config|
+ TweetStream.configure { |config|
   config.consumer_key        = "dnglKPpf09bOZs251d9LJBqkc"
-  config.consumer_secret     = "wAd8D1nElDx8x74cejeAWUKoV68a0EDGGL92075EipPl58IpS8"
+  config.consumer_secret      = "wAd8D1nElDx8x74cejeAWUKoV68a0EDGGL92075EipPl58IpS8"
   config.oauth_token        = "295014074-2IphEfxmXiAlM7xLrs2Ruu3wKbrYDjVASZLAfaah"
   config.oauth_token_secret = "JnT1aR5jTpBkj7ecpeZghrwDD8Ag5sdypQhMevmBpCyji"
   config.auth_method         = :oauth
+}
+
+
+class Tweets
+
+	# def showTweets
+	# # This will pull a sample of all tweets based on
+	# # your Twitter account's Streaming API role.
+	# TweetStream::Client.new.sample do |status|
+	# # The status object is a special Hash with
+	# # method access to its keys.
+	# 	puts "#{status.text}"
+	# 	end
+	# end
+
+# Use 'track' to track a list of single-word keywords
+	def trackTweets(trackWord1, trackWord2, trackWord3)
+		TweetStream::Client.new.track("#{trackWord1}", "#{trackWord2}", "#{trackWord3}") do |status|
+		  puts "#{status.text}" 
+		end
+	end
+
+	def error
+		break if @trackWord1 == 'MUFC'
+	end
+
+	# def stopStream
+	# # Stop after collecting 10 statuses
+	# @statuses = []
+	# TweetStream::Client.new.sample do |status, client|
+	# 	@statuses << status
+	# 	client.stop if @statuses.size >= 2
+	# 	end
+	# end
 end
 
-# This will pull a sample of all tweets based on
-# your Twitter account's Streaming API role.
-TweetStream::Client.new.sample do |status|
-  # The status object is a special Hash with
-  # method access to its keys.
-  puts "#{status.text}"
-end
 
-# # Use 'track' to track a list of single-word keywords
-# TweetStream::Client.new.track('InvictusGames') do |status|
-#   puts "#{status.text}"
-# end
 
-# # Use 'follow' to follow a group of user ids (integers, not screen names)
-# TweetStream::Client.new.follow(428333, 1652541, 34713362) do |status|
-#   puts "#{status.text}"
-# end
+puts "To track tweets word:  Tweets.new.trackTweets('enterYourWord')"
 
-# # Use 'userstream' to get message from your stream
-# class Tweet
-# 	def readTweet
-# 		client = TweetStream::Client.new
-
-# 		client.userstream do |status|
-# 			puts status.text
-# 		end
-# 	end
-# end
-
-# t = Tweet.new
-# t.readTweet
-# client = TweetStream::Client.new
-
-# client.sitestream(['115192457'], :followings => true) do |status|
-#   puts status.inspect
-# end
+t = Tweets.new
+t.trackTweets('MUFC', 'Chelsea', 'Arsenal')
